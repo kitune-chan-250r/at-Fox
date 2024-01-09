449231823200a1c50d78939e532f59b27cece3da
狐さん <41228665+kitune-chan-250r@users.noreply.github.com>
2024-01-09 23:33:56 +0900

    addFeedViewerのundefinedチェックを変更

--- front/src/utils/Utils.tsx
+++ front/src/utils/Utils.tsx
@@ -1,69 +1,85 @@
-import { BskyAgent, AtpSessionData, AppBskyFeedGetPostThread, AtpAgentOpts } from "@atproto/api";
-import { Response as TimelineResponse, QueryParams } from "@atproto/api/dist/client/types/app/bsky/feed/getTimeline"
+import {
+    BskyAgent,
+    AtpSessionData,
+    AppBskyFeedGetPostThread,
+    AtpAgentOpts,
+} from "@atproto/api";
+import {
+    Response as TimelineResponse,
+    QueryParams,
+} from "@atproto/api/dist/client/types/app/bsky/feed/getTimeline";
 import { FeedViewPost } from "@atproto/api/dist/client/types/app/bsky/feed/defs";
 import { SessionData } from "../types/SessionData";
-import { XRPCResponse } from '@atproto/xrpc';
+import { XRPCResponse } from "@atproto/xrpc";
 import { useCookies } from "react-cookie";
 
 /**
  * cookieを確認して有効なセッションデータが入っているか確認
  * @returns boolean
  */
-export const checkSessionData = (sessionData: AtpSessionData):boolean => {
+export const checkSessionData = (sessionData: AtpSessionData): boolean => {
     // console.info(sessionData);
     if (sessionData === undefined) {
         return false;
     }
     return true;
-}
-
+};
 
 /**
  * feedから指定したviewerの要素を取り除く
- * @param feed 
- * @param type 
+ * @param feed
+ * @param type
  * @returns FeedViewPost
  */
 export const deleteFeedViewer = (
-    feed: FeedViewPost, 
-    type: 'like' | 'repost'
+    feed: FeedViewPost,
+    type: "like" | "repost"
 ): FeedViewPost => {
-    if (type === 'like' && feed.post.viewer && feed.post.likeCount) {
+    if (type === "like" && feed.post.viewer && feed.post.likeCount) {
         delete feed.post.viewer.like; // プロパティを削除
         feed.post.likeCount -= 1;
-    } else if (type === 'repost' && feed.post.viewer && feed.post.repostCount) {
+    } else if (type === "repost" && feed.post.viewer && feed.post.repostCount) {
         delete feed.post.viewer.repost;
         feed.post.repostCount -= 1;
     }
     return feed;
-}
+};
 
 /**
  * feedから指定したviewerの要素を追加する
- * @param feed 
- * @param type 
+ * ここでカウントをプラス１するのやめた方がいいと思う
+ * @param feed
+ * @param type
  * @returns FeedViewPost
  */
 export const addFeedViewer = (
-    feed: FeedViewPost, 
-    type: 'like' | 'repost',
+    feed: FeedViewPost,
+    type: "like" | "repost",
     uri: string
 ): FeedViewPost => {
-    if (type === 'like' && feed.post.viewer && feed.post.likeCount) {
+    if (
+        type === "like" &&
+        feed.post.viewer !== undefined &&
+        feed.post.likeCount !== undefined
+    ) {
         feed.post.viewer.like = uri;
         feed.post.likeCount += 1;
-    } else if (type === 'repost' && feed.post.viewer && feed.post.repostCount) {
+    } else if (
+        type === "repost" &&
+        feed.post.viewer !== undefined &&
+        feed.post.repostCount !== undefined
+    ) {
         feed.post.viewer.repost = uri;
         feed.post.repostCount += 1;
     }
     return feed;
-}
+};
 
 /**
  * 改行やスペース、タブを受け取った文字列から排除する
  * @param text 改行やスペース、タブを消したい文字列
  * @returns 改行やスペース、タブが消えた文字列
  */
-export const deleteWhitespace = (text: string):string => {
-    return text.replace(/\s+/g, '');
-}
+export const deleteWhitespace = (text: string): string => {
+    return text.replace(/\s+/g, "");
+};
